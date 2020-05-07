10e5a79eff1de53f60c2603bdd310b96d0012e2e
Kęstutis Kaminskas <kestutis.kaminskas@icloud.com>
2020-05-07 16:43:20 +0900

    bricks[], brickCollision
    bricks[], brickCollision, minor fix with index

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 let canvas = document.getElementById("screen");
 let ctx = canvas.getContext("2d");
 
-
 // ball properties
 let x = 350;
 let y = 270;
@@ -22,6 +21,24 @@ let RIGHT_BUTTON = false;
 let LEFT_BUTTON = false;
 
 
+// brick wall properties
+let brickRowCount = 3;
+let brickCollumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+let bricks = [];
+let brickState = 1;
+// 2d brick array with empty xy coordinates
+// p.s. will be used for drawing and collission
+for (let i = 0; i < brickCollumnCount; i++) {
+    bricks[i] =[];
+    for (let j = 0; j < brickRowCount; j++) {
+        bricks[i][j] = { x: 0, y: 0, state: 1 };
+    }
+}
 
 
 // function getRandomInt(max) {
@@ -48,15 +65,53 @@ function drawPaddle() {
     ctx.closePath();
 }
 
-function checkCollision() {
+function drawBricks() {
+    for(let i = 0; i < brickCollumnCount; i++) {
+        for(let j = 0; j < brickRowCount; j++) {
+            if (bricks[i][j].state == 1) { // collision state
+                let brickX = (i * (brickWidth + brickPadding)) + brickOffsetLeft;
+                let brickY = (j * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[i][j].x = brickX;
+                bricks[i][j].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#ff8000";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function checkBallCollision() {
     // ball collision
     if (x + dx > canvas.width || x + dx < 0) { 
         dx = -dx;
         bump();
     }
+    // if (y + dy > paddleY + paddleWidth || + dy < paddleWidth) {
+    //     dy = -dy;
+    //     bump();
+    // }
     if (y + dy > canvas.height || y + dy < 0) {
         dy = -dy;
         bump();
+    }
+}
+
+function checkBrickCollision() {
+    for (let i = 0; i < brickCollumnCount; i++) {
+        for (let j = 0; j < brickRowCount; j++) {
+            let targetBrick = bricks[i][j];
+            if (targetBrick.state == 1) {
+                if (x > targetBrick.x && x < targetBrick.x + brickWidth && 
+                    y > targetBrick.y && y < targetBrick.y + brickHeight) {
+                        dy = -dy;
+                        targetBrick.state = 0;
+                        bump();
+                    }
+                }
+        }
     }
 }
 
@@ -84,14 +139,17 @@ function input() {
     }
 }
 
-// main game loop
+// game loop
 function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);   // re-draw
+    ctx.clearRect(0, 0, canvas.width, canvas.height);   // clear screen
     drawBall();
     drawPaddle();
+    checkBrickCollision();
+    drawBricks();
+
     x += dx;
     y += dy;
-    checkCollision();
+    checkBallCollision();
 
     // paddle movement and limits
     if (RIGHT_BUTTON) {
